0949c7c295194bef3afcc11bb1a29a55cfa9f5ec
henshim <hieume113@gmail.com>
2021-08-05 14:44:42 +0700

    hieu

--- web-food/database/seeders/RoleSeeder.php
+++ web-food/database/seeders/RoleSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
 use Illuminate\Database\Seeder;
 
 class RoleSeeder extends Seeder
@@ -13,6 +14,19 @@ class RoleSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $role = new Role();
+        $role->name='Admin';
+        $role->description='Web admin';
+        $role->save();
+
+        $role = new Role();
+        $role->name='Customer';
+        $role->description='Web customer';
+        $role->save();
+
+        $role = new Role();
+        $role->name='User';
+        $role->description='Web user';
+        $role->save();
     }
 }
