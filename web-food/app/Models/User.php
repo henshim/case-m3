fc9f295eab6ebbfdfccabfa5bb441ee5006f27ce
henshim <hieume113@gmail.com>
2021-08-05 11:46:30 +0700

    hieu

--- web-food/app/Models/User.php
+++ web-food/app/Models/User.php
@@ -11,6 +11,7 @@ class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
+    public $timestamps = false;
     /**
      * The attributes that are mass assignable.
      *
@@ -40,4 +41,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function role()
+    {
+        return $this->belongsToMany(Role::class);
+    }
+
+    public function food()
+    {
+        return $this->belongsTo(Food::class);
+    }
 }
--- web-food/database/back/2014_10_12_000000_create_users_table.php
+++ web-food/database/back/2014_10_12_000000_create_users_table.php
@@ -17,10 +17,18 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+//            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->text('img')->nullable()->default('default-avatar.jpg');
+            $table->string('status')->nullable();
+            $table->integer('phone');
+            $table->text('address')->nullable();
+//            $table->time('open_hour');
+            $table->string('forbidden')->nullable();
+            $table->unsignedBigInteger('role_id')->nullable();
+            $table->foreign('role_id')->references('id')->on('roles');
             $table->rememberToken();
-            $table->timestamps();
+            $table->string('restaurant')->nullable();
         });
     }
 
